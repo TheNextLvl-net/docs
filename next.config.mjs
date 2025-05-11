92059c8b0852f44c648225113f28a4fa6f46c464
set <lucameopou@gmail.com>
2025-05-11 11:20:02 +0200

    feat: add forwardings

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,39 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next"
 
-const withMDX = createMDX();
+const withMDX = createMDX()
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-};
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.com/invite/mDP9VWGeAw",
+        permanent: false,
+      },
+      {
+        source: "/youtube",
+        destination: "https://www.youtube.com/channel/UCHwqk5v8JSbpDb6frP4ZTOQ",
+        permanent: false,
+      },
+      {
+        source: "/namemc",
+        destination: "https://namemc.com/server/thenextlvl.net",
+        permanent: false,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/TheNextLvl-net/",
+        permanent: false,
+      },
+      {
+        source: "/plugins",
+        destination: "https://hangar.papermc.io/TheNextLvl",
+        permanent: false,
+      },
+    ]
+  },
+}
 
-export default withMDX(config);
+export default withMDX(config)
