19275ce3725e736d088c1886bb6e2b652cd35419
set <lucameopou@gmail.com>
2025-05-12 21:26:30 +0200

    refactor(source): simplify icon handling

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,17 +1,14 @@
 import { docs } from "@/.source"
 import { loader } from "fumadocs-core/source"
 import { createElement } from "react"
-import { DynamicIcon } from "lucide-react/dynamic"
-import dynamicIconImports from "lucide-react/dynamicIconImports"
+import { icons } from "lucide-react"
 export const source = loader({
   baseUrl: "/docs",
   source: docs.toFumadocsSource(),
-  icon(icon: string | undefined) {
-    if (icon && icon in dynamicIconImports) {
-      return createElement(DynamicIcon, {
-        name: icon as keyof typeof dynamicIconImports,
-      })
+  icon(icon) {
+    if (!icon) {
+      return
     }
-    return undefined
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons])
   },
 })
