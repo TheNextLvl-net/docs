b92f0836ba70f4123c1ca490923cb2f3c4f8fe9c
set <lucameopou@gmail.com>
2025-06-30 15:52:23 +0200

    fix: update analytics script to include domain and initialize plausible tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,14 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <Script
-          src={process.env.NEXT_PUBLIC_ANALYTICS_URL}
-          data-site-id={process.env.NEXT_PUBLIC_ANALYTICS_SITE_ID}
-          data-web-vitals="true"
-          data-track-errors="true"
           defer
+          data-domain={process.env.NEXT_PUBLIC_ANALYTICS_DOMAIN}
+          src={process.env.NEXT_PUBLIC_ANALYTICS_URL}
+        />
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`,
+          }}
         />
         <RootProvider>{children}</RootProvider>
       </body>
