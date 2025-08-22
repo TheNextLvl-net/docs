4e4627ecac9e172c58f762034347dfff817a3c8e
Luca <lucameopou@gmail.com>
2025-08-23 01:55:22 +0200

    feat: add sitemap generation functionality

---
+++ src/app/sitemap.ts
@@ -0,0 +1,31 @@
+import type { MetadataRoute } from 'next';
+import { source } from '@/lib/source';
+
+export const revalidate = false;
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const url = (path: string): string => new URL(path, process.env.NEXT_PUBLIC_BASE_URL).toString();
+
+  return [
+    {
+      url: url('/'),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: url('/docs'),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    ...(await Promise.all(
+      source.getPages().map(async (page) => {
+        return {
+          url: url(page.url),
+          lastModified: page.data.lastModified ? new Date(page.data.lastModified) : undefined,
+          changeFrequency: 'weekly',
+          priority: 0.5,
+        } as MetadataRoute.Sitemap[number];
+      }),
+    )),
+  ];
+}
