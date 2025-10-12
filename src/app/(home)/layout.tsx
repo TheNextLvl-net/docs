cef2fde1cbece68026074f497e79ff3436b84d6e
Luca <lucameopou@gmail.com>
2025-10-12 19:54:10 +0200

    chore: add Blog to home links

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react"
 import { HomeLayout } from "fumadocs-ui/layouts/home"
 import { baseOptions } from "@/app/layout.config"
 import { SiDiscord } from "@icons-pack/react-simple-icons"
+import { RssIcon } from "lucide-react"
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -10,9 +11,15 @@ export default function Layout({ children }: { children: ReactNode }) {
       links={[
         {
           type: "icon",
+          icon: <RssIcon/>,
+          text: "Blog",
+          url: "/blog"
+        },
+        {
+          type: "icon",
           icon: <SiDiscord />,
           text: "Discord",
-          url: "https://thenextlvl.net/discord",
+          url: "/discord",
         },
       ]}
     >
